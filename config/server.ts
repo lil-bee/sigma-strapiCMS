4b7158ce98e3a101c94bee4d57a539c622e35f68
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-06-17 16:19:34 +0700

    dari server

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  url: env('PUBLIC_URL', 'https://sigma-distribusi.com'),	
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
